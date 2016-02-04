223313f39b8633f38b0a39331f3a3cf772b1a9ff
Jamal Moir <jamal.moir@gmail.com>
2016-02-04 16:18:30 +0900

    Add 'add new exchange rate tracker' base code

---
+++ currency_report.py
@@ -0,0 +1,34 @@
+import sys
+import pickle
+import os.path
+from trackers import CurrencyTracker
+
+if len(sys.argv) == 0:
+    #TODO: currency updating and emailing
+elif len(sys.argv) == 2:
+    valid_currencies = open("currencies.txt").read()
+
+    if sys.argv[0] in valid_currencies and sys.argv[1] in valid_currencies:
+        currencies = (sys.argv[0], sys.argv[1])
+        new_tracker = CurrencyTracker(currencies, grab_rate(currencies))
+
+        if os.path.isfile("rates_to_watch.pkl"):
+            rates_to_watch = read_file("rates_to_watch.pkl")
+            rates_to_watch.append(new_tracker)
+            write_file(new_tracker, "rates_to_watch.pkl")
+else:
+    print("Error: Invalid number of arguments.")
+
+def read_file(file_name):
+    pkl = open(file_name)
+    contents = pickle.load(pkl)
+    pkl.close()
+    return contents
+
+def write_file(file_name, contents):
+    pkl = open(file_name, "w")
+    pickle.dump(contents, pkl)
+    pkl.close
+
+def grab_rate(currencies):
+
