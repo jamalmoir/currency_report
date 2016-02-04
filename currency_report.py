d9f967ef0909c348badfbac2ec9467bde588228b
Jamal Moir <jamal.moir@gmail.com>
2016-02-04 17:40:32 +0900

    Fix 'add new exchange rate tracker' logic

--- currency_report.py
+++ currency_report.py
@@ -3,21 +3,25 @@ import pickle
 import os.path
 from trackers import CurrencyTracker
 
-if len(sys.argv) == 0:
-    #TODO: currency updating and emailing
-elif len(sys.argv) == 2:
-    valid_currencies = open("currencies.txt").read()
+def main():
+    if len(sys.argv) == 0:
+        #TODO: currency updating and emailing
+    elif len(sys.argv) == 2:
+        valid_currencies = open("currencies.txt").read()
 
-    if sys.argv[0] in valid_currencies and sys.argv[1] in valid_currencies:
-        currencies = (sys.argv[0], sys.argv[1])
-        new_tracker = CurrencyTracker(currencies, grab_rate(currencies))
+        if sys.argv[0] in valid_currencies and sys.argv[1] in valid_currencies:
+            currencies = (sys.argv[0], sys.argv[1])
+            new_tracker = CurrencyTracker(currencies, grab_rate(currencies))
 
-        if os.path.isfile("rates_to_watch.pkl"):
-            rates_to_watch = read_file("rates_to_watch.pkl")
-            rates_to_watch.append(new_tracker)
-            write_file(new_tracker, "rates_to_watch.pkl")
-else:
-    print("Error: Invalid number of arguments.")
+            if os.path.isfile("rates_to_watch.pkl"):
+                rates_to_watch = read_file("rates_to_watch.pkl")
+                rates_to_watch.append(new_tracker)
+                write_file(rates_to_watch, "rates_to_watch.pkl")
+            else:
+                rates_to_watch = [new_tracker]
+                write_file(rates_to_watch, "rates_to_watch.pkl")
+    else:
+        print("Error: Invalid number of arguments.")
 
 def read_file(file_name):
     pkl = open(file_name)
