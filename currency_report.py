de437f56c615f618ee7c6df5c776de5ab80702f8
Jamal Moir <jamal.moir@gmail.com>
2016-02-05 21:24:07 +0900

    Fix exchange rate grabbing

--- currency_report.py
+++ currency_report.py
@@ -10,11 +10,11 @@ def main():
     if len(sys.argv) == 0:
         print("test")
         #TODO: currency updating and emailing
-    elif len(sys.argv) == 2:
+    elif len(sys.argv) == 3:
         valid_currencies = open("currencies.txt").read()
 
-        if sys.argv[0] in valid_currencies and sys.argv[1] in valid_currencies:
-            currencies = (sys.argv[0], sys.argv[1])
+        if sys.argv[1] in valid_currencies and sys.argv[2] in valid_currencies:
+            currencies = (sys.argv[1], sys.argv[2])
             new_tracker = CurrencyTracker(currencies, grab_rate(currencies))
 
             if os.path.isfile("rates_to_watch.pkl"):
@@ -27,7 +27,7 @@ def main():
         else:
             print("Error: Invalid currency codes.")
     else:
-        print("Error: Invalid number of arguments.")
+        print("Error: Invalid number of arguments. %s argument(s)." % (len(sys.argv)))
 
 def read_file(file_name):
     pkl = open(file_name, "rb")
@@ -42,8 +42,11 @@ def write_file(file_name, contents):
 
 def grab_rate(currencies):
     url = "http://finance.yahoo.com/d/quotes.csv?e=.csv&f=l1&s=%s%s=X" % (currencies[0], currencies[1])
-    response = urllib2.urlopen(url)
-    reader = csv.reader(response)
-    rate = (datetime.now(), reader.next())
+    response = urllib.request.urlopen(url)
+    data = response.read().decode("utf-8")
+    rate = (datetime.now(), data[:-1])
     print(rate)
     return rate
+
+if __name__ == "__main__":
+    main()
